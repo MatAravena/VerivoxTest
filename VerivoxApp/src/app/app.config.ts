1b1638a9072927246ce1378292add0401563ace1
Matu Aravena <matias.aravena.g@gmail.com>
2024-11-06 23:14:35 +0100

    Configuration router and added import for general used in projects

--- VerivoxApp/src/app/app.config.ts
+++ VerivoxApp/src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
+import { TariffService } from './services/tariff/tariff-service.service'; 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), 
+    provideClientHydration(),
+
+    ///New providers
+    provideHttpClient(),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, 
+        { 
+          dataEncapsulation: false,
+          apiBase: 'fake-api/'
+        }
+      )
+     ),
+    // Inner services, not sure if this is mandatory for the new angular 18
+    TariffService, InMemoryDataService
+  ]
 };
--- VerivoxApp/src/app/app.routes.ts
+++ VerivoxApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TariffListComponent } from './components/tariff-list/tariff-list.component';
+import { TariffCardComponent } from './components/tariff-card/tariff-card.component';
+import { TariffComparitionComponent } from './components/tariff-comparition/tariff-comparition.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , pathMatch: 'full', redirectTo: 'list'},
+    {path: 'list' , component: TariffListComponent },
+    {path: 'tariff' , component: TariffCardComponent },
+    {path: 'comparition' , component: TariffComparitionComponent },
+];
