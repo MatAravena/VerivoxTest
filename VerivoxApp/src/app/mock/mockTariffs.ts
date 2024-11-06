363e13fc2066f59e3dafac5c9b2723a2dd72670a
Matu Aravena <matias.aravena.g@gmail.com>
2024-11-06 23:15:36 +0100

    added mock values, basic readme, adapted basic structure in component

---
+++ VerivoxApp/src/app/mock/mockTariffs.ts
@@ -0,0 +1,39 @@
+import { Tariff } from "../models/tariff";
+
+export const MOCK_TARIFFS: Tariff[] = [
+    {
+        id: 1,
+        name: "Tari6 A",
+        price: 3.5,
+        supplier: "Company A",
+        description: "Description of Tari6 A"
+    }, 
+    {
+        id: 2,
+        name: "Tari6 B",
+        price: 4.0,
+        supplier: "Company B",
+        description: "Description of Tari6 B"
+    }, 
+    {
+        id: 3,
+        name: "Tari6 C",
+        price: 3.8,
+        supplier: "Company C",
+        description: "Description of Tari6 C"
+    }, 
+    {
+        id: 4,
+        name: "Tari6 D",
+        price: 6.2,
+        supplier: "Company D",
+        description: "Description of Tari6 D"
+    }, 
+    {
+        id: 5,
+        name: "Tari6 E",
+        price: 10,
+        supplier: "Company E",
+        description: "Description of Tari6 E"
+    }
+]
